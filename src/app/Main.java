ef07a02f789fa30a2fe11c8a11f78a348450cc91
LJF2402901363 <2402901363@qq.com>
2020-05-21 14:16:05 +0800

    首次提交自己实现的可进行加减乘除取模的计算器。

---
+++ src/app/Main.java
@@ -0,0 +1,17 @@
+package app;
+
+import view.Calculator;
+
+/**
+ * Classname:Main
+ *
+ * @description:
+ * @author: Ä°ÒâËæÓ°
+ * @Date: 2020-05-21 14:11
+ * @Version: 1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        new Calculator();
+    }
+}
